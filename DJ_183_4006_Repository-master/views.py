21b7eea139552001d37adf4c153cd52a773b207a
rcoseteng <43459823+rcoseteng@users.noreply.github.com>
2019-01-21 00:22:48 -0500

    Update views.py

--- DJ_183_4006_Repository-master/views.py
+++ DJ_183_4006_Repository-master/views.py
@@ -10,8 +10,6 @@ from A import B
 where A is a Python file in the /controllers folder (without the .py!) and B is
 the name of the function that runs that particular route.
 
-For every route, you must import it in this file. An example for the starter
-code is given below. Continue adding more as you make more routes.
 """
 
 from index import index
